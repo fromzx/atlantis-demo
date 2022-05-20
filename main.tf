6e8f798afe3e13adc7f913766852a273f9f33191
fromzx <wzhenxing@qq.com>
2022-05-20 09:57:56 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+data "template_file" "appconfig" {
+  template = file("${path.module}/template/appconfig.json")
+  vars = {
+    remoteServiceBaseUrl = "https://abc.com"
+    appBaseUrl           = "https://def.com"
+  }
+}
+
+resource "local_file" "appconfig" {
+  content         = data.template_file.appconfig.rendered
+  filename        = "/Users/jenwang/Desktop/appconfig.json"
+  file_permission = "0644"
+}
+
+resource "local_file" "appconfig_production" {
+  content         = data.template_file.appconfig.rendered
+  filename        = "/Users/jenwang/Desktop/appconfig.production.json"
+  file_permission = "0644"
+}
